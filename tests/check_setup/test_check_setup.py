5513565891bf7d000b9c89037f7262fad3889fac
James Trimarco <james.trimarco@gmail.com>
2021-02-01 09:16:17 -0500

    basic testing architecture

--- tests/check_setup/test_check_setup.py
+++ tests/check_setup/test_check_setup.py
@@ -1,2 +1,33 @@
 import pytest
 from pathlib import Path
+
+
+def assert_archive_exists(base_dir, sub_names):
+
+    directories = [base_dir / sub for sub in sub_names]
+    directories.append(base_dir)
+
+    for dir in directories:
+        print(dir)
+        assert dir.exists()
+
+
+class TestSetUpArchive:
+    def test_archive_missing(self, archive_dir):
+        """Tests set_up_archive() with the archive directory missing. It
+        expects the function to create the archive and sub directories"""
+
+        # setup
+        archive_dir.rmdir()
+        assert archive_dir.exists() == False
+
+        # execution
+        temp_dir = archive_dir.parent
+        passed, message, dir = set_up_archive(base_dir=temp_dir)
+
+        # validation
+        assert_dirs_exist(archive_dir, ARCHIVE_SUBS)
+        assert dir is not None
+        assert passed
+        assert message == "Archives folder is setup correctly"
+
---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+from pathlib import Path
+
+
+@pytest.fixture(scope="session")
+def private_dir(tmp_path_factory):
+    basetemp = Path.cwd() / "temp_dir"
+    os.environ["PYTEST_DEBUG_TEMPROOT"] = str(basetemp)
+    basetemp.mkdir(parents=True, exist_ok=True)
+    dir = tmp_path_factory.mktemp("private", numbered=False)
+    print(dir)
+    return dir
+
+
+@pytest.fixture(scope="session")
+def archives_dir(tmp_path_factory):
+    basetemp = Path.cwd() / "temp_dir"
+    os.environ["PYTEST_DEBUG_TEMPROOT"] = str(basetemp)
+    basetemp.mkdir(parents=True, exist_ok=True)
+    dir = tmp_path_factory.mktemp("archives", numbered=False)
+    print(dir)
+    return dir
