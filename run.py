4e15b52e1a2ec57e4e19a401638f8a5cb1d2fbd0
James Trimarco <james.trimarco@gmail.com>
2021-02-07 18:55:42 -0500

    incorporates changes to archive directory structure

--- run.py
+++ run.py
@@ -3,7 +3,11 @@ from pathlib import Path
 from utils.download_to_archive import DOWNLOAD_URL, scrape_salary_data, set_http_version
 
 if __name__ == "__main__":
+    # SCRAPING
+    set_http_version()
     # set path to archive
-    archive_path = Path.cwd() / "archives" / "archive_raw"
+    archive_path = Path.cwd() / "archive"
     scrape_salary_data(archive_path, DOWNLOAD_URL)
+
+    # BUILD DATABASE
 
