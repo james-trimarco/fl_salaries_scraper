13fbf7d7bda0bb1e7bcab3c18098ac32c463ee3a
James Trimarco <james.trimarco@gmail.com>
2021-02-08 21:21:29 -0500

    Fills out function to clean and cast types
    not done yet though

--- utils/format_table.py
+++ utils/format_table.py
@@ -5,7 +5,11 @@ from utils.df_formats import FORMATS
 # NON_NULLABLE = ["vendor_id", "document_number"]
 
 
-def format_table(df_raw, report):
+def numeric_string(series, fill):
+    return series.astype("float").astype("Int64").astype(str).str.zfill(fill)
+
+
+def format_table(df_raw):
     """Formats the input reports for merging and calculation
     Args:
         df_raw (dataframe): Input dataframe to be formatted
@@ -19,35 +23,40 @@ def format_table(df_raw, report):
     # check that the core report matches expected input format
     passed, errors = check_format(df_raw, input)
     if not passed:
-        error = f"{report} report doesn't match expected input format: {errors}"
+        error = f"Table report doesn't match expected input format: {errors}"
         return False, error, None
 
     # # clean report and standardize merge columns
-    # df = clean_report(df_raw, return_cols=return_cols)
-    # df["vendor_id"] = df["vendor_id"].astype("Int64").astype(str).str.zfill(8)
-    # df["document_number"] = df["document_number"].astype(str)
+    df = clean_report(df_raw)
+    df = df.rename(
+        columns={"employee_type": "is_salaried", "full/part_time": "is_full_time"}
+    )
+    df = df.applymap(lambda x: x.upper() if isinstance(x, str) else x)
 
-    # # do some extra formatting for core integrator
-    # if report == "core integrator":
-    #     # convert Julian date to datetime
-    #     julian = df["creation_date"] + 2415018
-    #     julian = pd.to_datetime(julian, unit="D", origin="julian")
-    #     df["creation_date"] = julian.dt.date.astype("datetime64")
-    #     # cast document_date to datetime
-    #     df["document_date"] = pd.to_datetime(df["document_date"])
-    #     # filter for invoices in DGS employee queue
-    #     cond_location = df["location"].isin(DGS_LOCATIONS)
-    #     cond_status = df["status"] == "Awaiting Agency Contact"
-    #     df = df[cond_location & cond_status].copy()
+    df["position_number"] = numeric_string(df["position_number"], 8)
+    df["class_code"] = numeric_string(df["class_code"], 5)
 
-    # # check that output dataframe
-    # passed, errors = check_format(df, output)
-    # if not passed:
-    #     error = f"{report} didn't get formatted correctly: {errors}"
-    #     return False, error, None
+    salary_map = {"is_salaried": {"SALARIED": True, "OPS": False}}
+    full_time_map = {"is_full_time": {"FULL TIME": True, "PART TIME": False}}
 
-    # message = f"Successfully formatted {report}"
-    # return True, message, df
+    df = df.replace(salary_map)
+    df = df.replace(full_time_map)
+
+    print(df.head())
+
+    # {'is_salaried': {'accepted': ['bool'], 'actual': 'object'},
+    # 'is_full_time': {'accepted': ['bool'], 'actual': 'object'},
+    # 'state_hire_date': {'accepted': ['datetime64[ns]'], 'actual': 'object'},
+    # 'ops_hourly_rate': {'accepted': ['float64'], 'actual': 'object'}}"
+
+    # check that output dataframe
+    passed, errors = check_format(df, output)
+    if not passed:
+        error = f"Table didn't get formatted correctly: {errors}"
+        return False, error, None
+
+    message = f"Successfully formatted Florida salaries table."
+    return True, message, df
 
 
 def check_format(df, format):
@@ -91,6 +100,7 @@ def clean_report(df_raw, na_cols=None, return_cols=None):
     Returns:
         df (dataframe): Clean dataframe
     """
+    print("cleaning the report")
 
     # make a copy of the columns to return
     if return_cols:
@@ -105,8 +115,5 @@ def clean_report(df_raw, na_cols=None, return_cols=None):
 
     # remove whitespaces around string columns
     df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-
-    # drop rows with data missing
-    df.dropna(subset=na_cols, inplace=True)
 
     return df
